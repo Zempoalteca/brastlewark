6885d86d11f370965d4d7c9de6642e049b4d0fe6
Gabriel Zempoalteca <gabriel.zmpltc@gmail.com>
2020-07-18 17:44:12 -0500

    feat: Welcome view
    Use lotties to show welcome view
    Next files for animations were added:
    Lotties:
    - trolling.json animation
    - background-waves.json animation
    - background-waves_2.json animation
    - loading.json animation

---
+++ Brastlewark/Brastlewark/Modules/WelcomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WelcomeViewController.swift
+//  Brastlewark
+//
+//  Created by Gabriel Z on 18/07/20.
+//  Copyright © 2020 Zempoalteca. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var trollingView: AnimationView!
+    @IBOutlet weak var wavesView: AnimationView!
+    @IBOutlet weak var lblWelcome: UILabel!
+
+    // MARK: - Variables
+
+    var animationView: AnimationView?
+    var backgroundWaves: AnimationView?
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initComponents()
+    }
+
+    // MARK: - Functions
+
+    private func initComponents() {
+        lblWelcome.text = "Welcome to \rBRASTLEWARK"
+        trollingView.play()
+        trollingView.loopMode = .loop
+        backgroundWaves?.play()
+        backgroundWaves?.loopMode = .loop
+    }
+
+}
